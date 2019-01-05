6f79e637492f068115fd2fb80c383323d250a803
Michael Stone <mikepwns37@gmail.com>
2019-01-05 00:15:37 -0500

    Add printing lines and comments

--- voteAgeCheck.js
+++ voteAgeCheck.js
@@ -1,9 +1,13 @@
 const canIVote = age => {
-  if (age >= 18) {
-    return true;
-  } else {
-    return false;
-  }
+	console.log('Welcome to the voting age checker!');
+	// Check if user is at least 18 years old
+	if (age >= 18) {
+		// If they are, display that they are able to vote
+  		return 'You are ' + age + ' years old. Yes, you can vote!';
+  	} else {
+  		// If they're not, display that they aren't able to vote
+    	return 'You are only ' + age + ' years old. No, you cannote vote.';
+  	}
 }
 
 console.log(canIVote(19));
